169a7b76454d226e4006565c6fe647f1eaa55f2a
spr-k <spkaufmann@outlook.com>
2023-06-26 15:07:48 -0400

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
